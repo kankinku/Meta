089c0853fe5ddb9e1b0b429833afe2548897d81b
kankinku <wlsdn1232580@gmail.com>
2024-02-29 15:01:24 +0900

    make lib

---
+++ c/calc.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "libmy.h"
+
+int main() {
+  int x, y;
+  scanf("%d %d", &x, &y);
+  printf("%d + %d = %d\n", x, y, plus(x, y));
+  printf("%d - %d = %d\n", x, y, minus(x, y));
+  return 0;
+}
